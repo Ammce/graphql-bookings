4b0e3072d6b49825b3a4530451cc8e2453fa1f9a
Amel <amcenp@gmail.com>
2019-04-02 23:32:39 +0200

    A little bit of code organization

--- resolvers/index.js
+++ resolvers/index.js
@@ -4,8 +4,17 @@ import bcrypt from 'bcrypt';
 import Event from '../models/event';
 import User from '../models/user';
 import Booking from '../models/booking';
+import { dateToStringHelper } from '../helpers/date';
 
 let customUserId = "5c9ab6d210ace82f6378b4c9";
+
+const transformEvent = event => {
+    return {
+        ...event._doc,
+        date: dateToStringHelper(event._doc.date),
+        creator: user.bind(this, event.creator)
+    }
+}
 
 const user = async userId => {
     let creator = await User.findById(userId)
@@ -18,59 +27,57 @@ const user = async userId => {
 const events = async eventIds => {
     let createdEvents = await Event.find({ _id: { $in: eventIds } })
     return createdEvents.map(event => {
-        return {
-            ...event._doc,
-            creator: user
-        }
+        return transformEvent(event);
     })
 }
 
+
 const singleEvent = async eventId => {
     let createdEvent = await Event.findById(eventId);
-    return {
-        ...createdEvent._doc,
-        creator: user.bind(this, createdEvent.creator)
-    }
+    return transformEvent(createdEvent);
 }
+
+
 
 export default {
     async events() {
         try {
             let allEvents = await Event.find();
             return allEvents.map(event => {
-                return {
-                    ...event._doc,
-                    creator: user.bind(this, event.creator)
-                }
+                return transformEvent(event);
             })
         } catch (e) {
-            console.log(e);
             throw new Error(e);
         }
     },
     async bookings() {
         try {
-            return await Booking.find();
+            const bookings = await Booking.find();
+            return bookings.map(booking => {
+                return {
+                    ...booking._doc,
+                    createdAt: dateToStringHelper(booking._doc.createdAt),
+                    updatedAt: dateToStringHelper(booking._doc.updatedAt),
+                    user: user.bind(this, booking._doc.user),
+                    event: singleEvent.bind(this, booking._doc.event),
+                }
+            })
         } catch (error) {
             throw new Error(error);
         }
     },
     async createEvent(args) {
         try {
-            console.log(args.data.date)
             const event = new Event({
                 ...args.data,
-                date: new Date(args.data.date)
+                date: dateToStringHelper(args.data.date)
             });
             let eventSaved = await event.save();
             let findCreator = await User.findOneAndUpdate({ _id: customUserId }, { $push: { createdEvents: eventSaved } }, { safe: true, upsert: true });
             // if (findCreator) {
             //     findCreator.createdEvents.push(eventSaved);
             // }
-            return {
-                ...eventSaved._doc,
-                creator: user.bind(this, findCreator._id)
-            };
+            return transformEvent(eventSaved);
         } catch (e) {
             console.log(e);
             throw new Error(e);
@@ -105,8 +112,8 @@ export default {
             let savedBooking = await newBooking.save();
             return {
                 ...savedBooking._doc,
-                createdAt: new Date(savedBooking._doc.createdAt).toISOString(),
-                updatedAt: new Date(savedBooking._doc.updatedAt).toISOString(),
+                createdAt: dateToStringHelper(savedBooking._doc.createdAt).toISOString(),
+                updatedAt: dateToStringHelper(savedBooking._doc.updatedAt).toISOString(),
                 user: user.bind(this, savedBooking.user),
                 event: singleEvent.bind(this, eventId),
             }
@@ -120,10 +127,7 @@ export default {
             const eventId = deletedBooking._doc.event;
             let result = await Booking.deleteOne({ _id: bookingId });
             let deletedEvent = await Event.findById(eventId);
-            return {
-                ...deletedEvent._doc,
-                creator: user.bind(this, customUserId)
-            }
+            return transformEvent(deletedEvent);
         } catch (error) {
             throw new Error(error);
         }
