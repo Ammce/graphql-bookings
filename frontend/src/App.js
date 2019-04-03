395432b0ba05d95ff479a33da65545d1ef0f6213
Amel Muminovic <amcenp@gmail.com>
2019-04-03 17:44:10 +0200

    Router added and navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import './App.css';
+
+// Importing Component
+import Auth from './pages/Auth';
+import Bookings from './pages/Bookings';
+import Events from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Working</h1>
-      </div>
+      <BrowserRouter>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect path="/" to='/auth' exact/>
+              <Route path="/auth" component={Auth} />
+              <Route path="/events" component={Events} />
+              <Route path="/bookings" component={Bookings} />
+            </Switch>
+          </main>
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
